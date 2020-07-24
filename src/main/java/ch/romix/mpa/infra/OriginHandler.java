56c69f7e453b30ffb798270befe5363f52e50fc0
Roman Schaller <roman.schaller@gmail.com>
2020-07-24 22:15:01 +0200

    Remove unused injected vert.x

--- src/main/java/ch/romix/mpa/infra/OriginHandler.java
+++ src/main/java/ch/romix/mpa/infra/OriginHandler.java
@@ -1,21 +1,16 @@
 package ch.romix.mpa.infra;
 
-import io.vertx.core.Vertx;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import java.net.URI;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
-import javax.inject.Inject;
 import javax.ws.rs.core.UriBuilder;
 import org.slf4j.LoggerFactory;
 
 @ApplicationScoped
 public class OriginHandler {
   private static org.slf4j.Logger LOGGER = LoggerFactory.getLogger(OriginHandler.class);
-
-  @Inject
-  Vertx vertx;
 
   public void init(@Observes Router router) {
     router.route().order(1).handler(rc -> {
