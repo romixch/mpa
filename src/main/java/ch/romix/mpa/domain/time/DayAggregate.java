fe5beeffc59d45c0c323da732791e397c0e0f3df
Roman Schaller <roman.schaller@gmail.com>
2020-10-04 23:13:41 +0200

    Add daily summary

--- src/main/java/ch/romix/mpa/domain/time/DayAggregate.java
+++ src/main/java/ch/romix/mpa/domain/time/DayAggregate.java
@@ -3,6 +3,7 @@ package ch.romix.mpa.domain.time;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.util.Collection;
+import java.util.Collections;
 
 public class DayAggregate {
   private final LocalDate day;
@@ -13,5 +14,17 @@ public class DayAggregate {
     this.day = day;
     this.duration = entries.stream().map(e -> e.duration).reduce(Duration.ZERO, Duration::plus);
     this.entries = entries;
+  }
+
+  public Collection<EntryEntity> getEntries() {
+    return Collections.unmodifiableCollection(entries);
+  }
+
+  public LocalDate getDay() {
+    return day;
+  }
+
+  public Duration getDuration() {
+    return duration;
   }
 }
--- src/main/java/ch/romix/mpa/web/time/TimeRoute.java
+++ src/main/java/ch/romix/mpa/web/time/TimeRoute.java
@@ -1,8 +1,10 @@
 package ch.romix.mpa.web.time;
 
 import ch.romix.mpa.domain.UserValueObject;
+import ch.romix.mpa.domain.time.DayAggregate;
 import ch.romix.mpa.domain.time.EntryEntity;
 import ch.romix.mpa.domain.time.EntryRepository;
+import ch.romix.mpa.domain.time.EntryService;
 import ch.romix.mpa.infra.OriginHandler;
 import io.quarkus.qute.Template;
 import io.quarkus.qute.api.ResourcePath;
@@ -12,6 +14,7 @@ import io.vertx.ext.web.RoutingContext;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.UUID;
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -31,9 +34,11 @@ public class TimeRoute {
   public void index(RoutingContext rc) throws URISyntaxException {
     try {
       UserValueObject user = rc.session().get("user");
+      Collection<EntryEntity> entries = entryRepository.findByUser(rc.session().get("user"));
+      Collection<DayAggregate> dailyAggregates = EntryService.aggregateByDate(entries);
       rc.response().end(index
           .data("user", user)
-          .data("entries", entryRepository.findByUser(rc.session().get("user")))
+          .data("dailyAggregates", dailyAggregates)
           .data("currentDay", LocalDate.now())
           .render());
     } catch (ClassCastException ex) {
